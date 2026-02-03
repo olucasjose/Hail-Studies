71c4afa9eebd5c5cdf933300bda7c0c1a522759e
Lucas José <65680721+olucasjose@users.noreply.github.com>
2026-02-03 00:10:48 +0000

    Atualiza versionCode e versionName para 1.14.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.aistra.hail"
         minSdk = 23
         targetSdk = 36
-        versionCode = 35
-        versionName = "1.11.0"
+        versionCode = 36
+        versionName = "1.14.0"
     }
 
     buildTypes {
